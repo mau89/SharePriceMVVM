a2fff5297d4b0dba8fa88b79af5fb169daa57c4a
mau89 <mau0689@gmail.com>
2021-09-27 23:12:25 +0500

    added implementation and module

---
+++ app/src/main/java/com/example/sharepricemvvm/screens/DescriptionsActionFragment.kt
@@ -0,0 +1,6 @@
+package com.example.sharepricemvvm.screens
+
+import androidx.fragment.app.Fragment
+import com.example.sharepricemvvm.R
+
+class DescriptionsActionFragment: Fragment(R.layout.fragment_descriptions_action)
