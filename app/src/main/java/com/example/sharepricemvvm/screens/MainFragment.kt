35597e49f94dde26da009dd5a4680876d3acae14
mau89 <mau0689@gmail.com>
2021-09-30 00:24:44 +0500

    added retrofit

--- app/src/main/java/com/example/sharepricemvvm/screens/MainFragment.kt
+++ app/src/main/java/com/example/sharepricemvvm/screens/MainFragment.kt
@@ -1,38 +1,23 @@
 package com.example.sharepricemvvm.screens
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sharepricemvvm.R
 import com.example.sharepricemvvm.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
 
-    private var _binding: FragmentMainBinding? = null
-
-    private val binding get() = _binding!!
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    private lateinit var binding: FragmentMainBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
         binding.goToDescription.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_descriptionsAction)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
---
+++ data/src/main/java/com/example/data/getallactions/GetAllActions.kt
@@ -0,0 +1,5 @@
+package com.example.data.getallactions
+
+class GetAllActions {
+
+}
